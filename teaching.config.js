7c3cacb950a6c965f6e12bbd48bd407f6e9aafa0
ooi yuan shyong <shyong224@gmail.com>
2026-09-24 17:38:09 +0800

    Fix link formatting in teaching.config.js

--- teaching.config.js
+++ teaching.config.js
@@ -180,7 +180,7 @@ const teachingConfig = {
       event: "NCTS-IBS CGP Joint Workshop 2026",
       title: "TBA",
       location: "Taipei, Taiwan",
-      link: "https://ncts.ntu.edu.tw/events_2_detail.php?nid=584
+      link: "https://ncts.ntu.edu.tw/events_2_detail.php?nid=584",
       type: "upcoming"
     },
     {
